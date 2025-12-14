9c5429d0a4c062f5a05e35c7b7121f99a49504dc
elizavetalartsenko-crypto <elizaveta.lartsenko@gmail.com>
2025-12-14 21:10:56 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -9,12 +9,12 @@ FROM customers;
 SELECT
     e.first_name || ' ' || e.last_name AS seller,
     COUNT(s.quantity) AS operations,
-    ROUND(s.quantity*p.price, 0) AS income
+    s.quantity*p.price AS income
 FROM sales s
 JOIN employees e
 ON s.sales_person_id = e.employee_id
-JOIN products AS p
-ON s.product_id = p.product_id 
+join products as p
+on s.product_id = p.product_id 
 GROUP BY seller, s.quantity, p.price
 ORDER BY income DESC
 LIMIT 10;
@@ -51,3 +51,53 @@ JOIN employees e ON s.sales_person_id = e.employee_id
 JOIN products p ON s.product_id = p.product_id
 GROUP BY seller, day_of_week, EXTRACT(DOW FROM s.sale_date)
 ORDER BY EXTRACT(DOW FROM s.sale_date), seller;
+
+
+-- Покупатели по возрастным группам
+SELECT age_category, COUNT(*) AS age_count
+FROM (
+    SELECT
+        CASE
+            WHEN age BETWEEN 16 AND 25 THEN '16-25'
+            WHEN age BETWEEN 26 AND 40 THEN '26-40'
+            ELSE '40+'
+        END AS age_category
+    FROM customers
+) AS sub
+GROUP BY age_category
+ORDER BY
+    CASE
+        WHEN age_category = '16-25' THEN 1
+        WHEN age_category = '26-40' THEN 2
+        ELSE 3
+    END;
+
+
+-- Уникальные покупатели и выручка по месяцам
+SELECT
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+    COUNT(DISTINCT s.customer_id) AS total_customers,
+    SUM(s.quantity * p.price) AS income
+FROM sales s
+JOIN products p ON s.product_id = p.product_id
+GROUP BY selling_month
+ORDER BY selling_month ASC;
+
+
+-- Покупатели с первой покупкой акционного товара
+WITH first_sales AS (
+    SELECT s.customer_id, MIN(s.sale_date) AS first_sale_date
+    FROM sales s
+    JOIN products p ON s.product_id = p.product_id
+    WHERE p.price = 0
+    GROUP BY s.customer_id
+)
+SELECT
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    fs.first_sale_date AS sale_date,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
+FROM first_sales fs
+JOIN sales s ON s.customer_id = fs.customer_id AND s.sale_date = fs.first_sale_date
+JOIN customers c ON c.customer_id = fs.customer_id
+JOIN employees e ON e.employee_id = s.sales_person_id
+ORDER BY fs.customer_id;
