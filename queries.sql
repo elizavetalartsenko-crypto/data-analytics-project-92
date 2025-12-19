5ce853dd71f944635c81280d009a2bb28248a642
elizavetalartsenko-crypto <elizaveta.lartsenko@gmail.com>
2025-12-19 21:19:16 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,9 +10,9 @@ SELECT
     COUNT(s.sales_id) AS operations,
     FLOOR(SUM(s.quantity * p.price)) AS income
 FROM sales AS s
-JOIN employees AS e
+INNER JOIN employees AS e
     ON e.employee_id = s.sales_person_id
-JOIN products AS p
+INNER JOIN products AS p
     ON p.product_id = s.product_id
 GROUP BY
     e.employee_id,
@@ -23,33 +23,38 @@ ORDER BY
 LIMIT 10;
 
 
-
 -- Продавцы с ниже средней выручкой за сделку, округление вниз
 SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS seller,
     FLOOR(AVG(s.quantity * p.price)) AS average_income
 FROM sales AS s
-JOIN employees AS e ON s.sales_person_id = e.employee_id
-JOIN products AS p ON s.product_id = p.product_id
-GROUP BY e.employee_id, e.first_name, e.last_name
+INNER JOIN employees AS e
+    ON s.sales_person_id = e.employee_id
+INNER JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY
+    e.employee_id,
+    e.first_name,
+    e.last_name
 HAVING AVG(s.quantity * p.price) < (
     SELECT AVG(s1.quantity * p1.price)
     FROM sales AS s1
-    JOIN products AS p1 ON s1.product_id = p1.product_id
+    INNER JOIN products AS p1
+        ON s1.product_id = p1.product_id
 )
-ORDER BY average_income ASC;
-
+ORDER BY
+    average_income ASC;
 
 
 -- Выручка продавцов по дням недели
 SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS seller,
-    TRIM(LOWER(TO_CHAR(s.sale_date, 'Day'))) AS day_of_week,
+    TRIM(TO_CHAR(s.sale_date, 'Day')) AS day_of_week,
     FLOOR(SUM(s.quantity * p.price)) AS income
 FROM sales AS s
-JOIN employees AS e
+INNER JOIN employees AS e
     ON e.employee_id = s.sales_person_id
-JOIN products AS p
+INNER JOIN products AS p
     ON p.product_id = s.product_id
 GROUP BY
     e.employee_id,
@@ -65,9 +70,6 @@ ORDER BY
     seller;
 
 
-
-
-
 -- Покупатели по возрастным группам
 SELECT
     age_category,
@@ -81,7 +83,8 @@ FROM (
         END AS age_category
     FROM customers AS c
 ) AS sub
-GROUP BY age_category
+GROUP BY
+    age_category
 ORDER BY
     CASE
         WHEN age_category = '16-25' THEN 1
@@ -96,13 +99,12 @@ SELECT
     COUNT(DISTINCT s.customer_id) AS total_customers,
     FLOOR(SUM(s.quantity * p.price)) AS income
 FROM sales AS s
-JOIN products AS p
+INNER JOIN products AS p
     ON p.product_id = s.product_id
 GROUP BY
     TO_CHAR(s.sale_date, 'YYYY-MM')
 ORDER BY
     selling_month;
-
 
 
 -- Покупатели с первой покупкой акционного товара
@@ -126,8 +128,11 @@ SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS seller
 FROM first_sales AS fs
 INNER JOIN customers AS c
-    ON c.customer_id = fs.customer_id
+    ON fs.customer_id = c.customer_id
 INNER JOIN employees AS e
-    ON e.employee_id = fs.sales_person_id
+    ON fs.sales_person_id = e.employee_id
 WHERE fs.rn = 1
-ORDER BY fs.customer_id;
+ORDER BY
+    fs.customer_id;
+
+
